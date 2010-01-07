fdbfe7f0fe54e12a933575e31503d75582492ebe
Ben@balder.aldaviva.com <Ben@balder.aldaviva.com>
2010-01-07 03:22:31 -0500

    Gallery and Photo model scaffolded

---
+++ application/models/gallery.php
@@ -0,0 +1,10 @@
+<?php
+
+	class Gallery_Model extends ORM {
+
+		protected $has_one = array('photo');
+		protected $has_many = array('photos');
+
+	}
+
+?>
---
+++ application/models/photo.php
@@ -0,0 +1,9 @@
+<?php
+
+	class Photo_Model extends ORM {
+
+		protected $belongs_to = array('gallery');
+
+	}
+
+?>
