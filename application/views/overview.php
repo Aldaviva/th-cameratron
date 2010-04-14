614fa0bbc7aa00934add22f538b8e266f23bd934
Ben Hutchison <ben@thor.aldaviva.com>
2010-04-14 00:04:30 -0400

    Ellipses in polaroids worked great. Now to make the metadata touch the screen corners (undo previous CSS wizardry).

--- application/views/overview.php
+++ application/views/overview.php
@@ -22,11 +22,8 @@ foreach($galleries as $gallery){
 	echo html::anchor($linkURL,
 			"<div class='polaroid'>"
 			. "<div class='poster_photo'><div>".$posterImgTag."</div></div>"
-			//. "<h3>".text::limit_chars($gallery->title, $title_max_chars)."</h3>"
-			. " <div class='expand'>"
-			. "  <h3>{$gallery->title}</h3>"
+			. "  <h3 class='ellipses'>{$gallery->title}</h3>"
 			. "  <div class='date'>".date('F Y', $gallery->date)."</div>"
-			. " </div>"
 			. "</div>",
 		array('title' => $gallery->title, 'class' => 'gallery_link')
 		);
